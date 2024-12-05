3cabcb7d293feac84b7b97f1914902b56392c4e1
longv <hailongvt1997@gmail.com>
2024-12-05 21:27:10 +0700

    Init project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.ouroboros.sleepingqueen {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.ouroboros.sleepingqueen to javafx.fxml;
+    exports com.ouroboros.sleepingqueen;
+}
