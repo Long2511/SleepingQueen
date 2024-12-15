17cb3b05bf2e5827f5bf1c4ad92fe192104a36d8
longv <hailongvt1997@gmail.com>
2024-12-16 05:25:05 +0700

    add new boards

---
+++ src/main/java/com/ouroboros/sleepingqueen/BoardViewController.java
@@ -0,0 +1,50 @@
+package com.ouroboros.sleepingqueen;
+
+import com.ouroboros.sleepingqueen.deck.Card;
+import com.ouroboros.sleepingqueen.deck.CardDeck;
+import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.control.SplitPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import java.util.Objects;
+
+public class BoardViewController {
+    @FXML
+    private GridPane centerPlayingBoard;
+
+    @FXML
+    public SplitPane splitPaneLeft;
+
+    @FXML
+    public void initialize() {
+        splitPaneLeft.getStyleClass().add("static-split");
+
+        CardDeck cardDeck = new CardDeck();
+        addCardsToGridPane(centerPlayingBoard, cardDeck);
+
+        for (Node splitPaneDivider : splitPaneLeft.lookupAll(".split-pane-divider")) {
+            splitPaneDivider.setMouseTransparent(true);
+        }
+    }
+
+    private Image getImage(Card card) {
+        return new Image(Objects.requireNonNull(BoardViewController.class.getResourceAsStream(card.getCardImgPath())));
+    }
+
+    private void addCardsToGridPane(GridPane gridPane, CardDeck cardDeck) {
+        int row = 0;
+        int col = 1;
+
+        ImageView imageView = new ImageView(getImage(cardDeck.draw()));
+        imageView.setFitWidth(100);
+        imageView.setFitHeight(150);
+        GridPane.setHalignment(imageView, HPos.CENTER);
+        GridPane.setValignment(imageView, VPos.CENTER);
+
+        gridPane.add(imageView, col, row);
+    }
+}
---
+++ src/main/java/com/ouroboros/sleepingqueen/PreGameScreenController.java
@@ -0,0 +1,35 @@
+package com.ouroboros.sleepingqueen;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PreGameScreenController  {
+    @FXML
+    private void handleBackButtonAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/ouroboros/sleepingqueen/view/main-screen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void handleJoinGameButtonAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/ouroboros/sleepingqueen/view/board-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+}
