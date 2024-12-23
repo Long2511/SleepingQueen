7a074a2ee06d88db09e235c6bc804ce4b3bb32a5
longv <hailongvt1997@gmail.com>
2024-12-24 05:56:10 +0700

    Update main screen to remove screen border

--- src/main/java/com/ouroboros/sleepingqueen/MainScreen.java
+++ src/main/java/com/ouroboros/sleepingqueen/MainScreen.java
@@ -1,30 +1,48 @@
 package com.ouroboros.sleepingqueen;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class MainScreen extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainScreen.class.getResource("view/board-view.fxml"));
 //        FXMLLoader fxmlLoader = new FXMLLoader(MainScreen.class.getResource("view/main-screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
 
+
+        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        stage.setX(event.getScreenX() - scene.getWindow().getWidth() / 2);
+                        stage.setY(event.getScreenY() - 20);
+                    }
+                });
+            }
+        });
+
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setTitle("Sleeping Queen");
         stage.getIcons().add(new Image(Objects.requireNonNull(MainScreen.class.getResourceAsStream("/com/ouroboros/sleepingqueen/icon/icon.png"))));
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 
 }
