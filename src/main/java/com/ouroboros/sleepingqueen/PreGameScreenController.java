dce85dd7142cc17e97d408467f28e53a906d7ca0
Smile030504 <lthanhphuc0305@gmail.com>
2024-12-30 02:21:31 +0100

    Fix the PreGameScreenController.java and board-view.fxml, change the menu UI to show full word "Menu"

--- src/main/java/com/ouroboros/sleepingqueen/PreGameScreenController.java
+++ src/main/java/com/ouroboros/sleepingqueen/PreGameScreenController.java
@@ -1,11 +1,20 @@
 package com.ouroboros.sleepingqueen;
 
+import com.ouroboros.sleepingqueen.multiplayer.GameClient;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
-public class PreGameScreenController  {
+public class PreGameScreenController {
+
+    @FXML
+    private TextField playerNameField;
+
+    @FXML
+    private TextField gameIdField;
+
     @FXML
     private void handleBackButtonAction(ActionEvent event) throws IOException {
         SceneChanger.changeScene(event, "/com/ouroboros/sleepingqueen/view/main-screen.fxml");
@@ -13,6 +22,14 @@ public class PreGameScreenController  {
 
     @FXML
     private void handleJoinGameButtonAction(ActionEvent event) throws IOException {
+        String playerName = playerNameField.getText();
+        String gameId = gameIdField.getText();
+        new GameClient().startClient(playerName, gameId, 0);
+        SceneChanger.changeScene(event, "/com/ouroboros/sleepingqueen/view/board-view.fxml");
+    }
+
+    @FXML
+    private void handleCreateGameButtonAction(ActionEvent event) throws IOException {
         SceneChanger.changeScene(event, "/com/ouroboros/sleepingqueen/view/board-view.fxml");
     }
 }
