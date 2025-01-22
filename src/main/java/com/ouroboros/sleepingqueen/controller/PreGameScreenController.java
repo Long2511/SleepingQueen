3f533d8cd1b4468b9a4576ea47542b03d91fb351
longv <hailongvt1997@gmail.com>
2025-01-22 23:48:52 +0700

    Add checkbox for selecting total players

--- src/main/java/com/ouroboros/sleepingqueen/controller/PreGameScreenController.java
+++ src/main/java/com/ouroboros/sleepingqueen/controller/PreGameScreenController.java
@@ -2,33 +2,31 @@ package com.ouroboros.sleepingqueen.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
 
 import java.io.IOException;
 
 public class PreGameScreenController {
 
     @FXML
-    private TextField playerNameField;
+    private ComboBox playerCountComboBox;
 
-    @FXML
-    private TextField gameIdField;
 
     @FXML
     private void handleBackButtonAction(ActionEvent event) throws IOException {
         SceneChanger.changeScene(event, "/com/ouroboros/sleepingqueen/view/main-screen.fxml");
     }
 
-    @FXML
-    private void handleJoinGameButtonAction(ActionEvent event) throws IOException {
-/*        String playerName = playerNameField.getText();
-        String gameId = gameIdField.getText();
-        new GameClient().startClient(playerName, gameId, 0);*/
-        SceneChanger.changeScene(event, "/com/ouroboros/sleepingqueen/view/board-view.fxml");
-    }
 
     @FXML
     private void handleCreateGameButtonAction(ActionEvent event) throws IOException {
         SceneChanger.changeScene(event, "/com/ouroboros/sleepingqueen/view/board-view.fxml");
     }
+
+    public void initialize() {
+        playerCountComboBox.getItems().removeAll(playerCountComboBox.getItems());
+        playerCountComboBox.getItems().addAll("2 Players", "3 Players", "4 Players", "5 Players");
+        playerCountComboBox.getSelectionModel().select("2 Players");
+    }
+
 }
