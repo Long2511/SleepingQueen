3262c5ca5a4060cb680891767abca965320977e1
longv <hailongvt1997@gmail.com>
2025-01-23 00:36:00 +0700

    add all file

--- src/main/java/com/ouroboros/sleepingqueen/controller/BoardViewController.java
+++ src/main/java/com/ouroboros/sleepingqueen/controller/BoardViewController.java
@@ -3,13 +3,21 @@ package com.ouroboros.sleepingqueen.controller;
 import com.ouroboros.sleepingqueen.card.DeckController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 public class BoardViewController {
+    private static String playerCount;
 
+    private DeckController deckController;
+    @FXML
+    private StackPane rootPane;
     @FXML
     private HBox deckField;
+    @FXML
+    private AnchorPane overlayPane;
 
     public static String getPlayerCount() {
         return playerCount;
@@ -21,6 +29,7 @@ public class BoardViewController {
 
     @FXML
     public void initialize() {
+
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/ouroboros/sleepingqueen/view/boardView/deck-on-board.fxml"));
             VBox deckPane = loader.load();
@@ -28,7 +37,17 @@ public class BoardViewController {
             deckController = loader.getController();
             deckController.setPlayNowButtonClick(this::handlePlayNowButtonClick);
 
+            // Load overlay
+            FXMLLoader overlayLoader = new FXMLLoader(getClass().getResource("/com/ouroboros/sleepingqueen/view/menu.fxml"));
+            overlayPane = overlayLoader.load();
+            overlayPane.setVisible(false);
+            deckField.getChildren().add(overlayPane);
 
+            // Set overlay constraints
+            AnchorPane.setTopAnchor(overlayPane, 0.0);
+            AnchorPane.setBottomAnchor(overlayPane, 0.0);
+            AnchorPane.setLeftAnchor(overlayPane, 0.0);
+            AnchorPane.setRightAnchor(overlayPane, 0.0);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -36,7 +55,10 @@ public class BoardViewController {
 
     private void handlePlayNowButtonClick() {
         System.out.println("Play now button clicked");
+    }
 
+    private void handleMenuButtonClick() {
+        overlayPane.setVisible(true);
     }
 
 
